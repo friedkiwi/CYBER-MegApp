27902a2f742b3b26e47d439c5bb45aa50da0888c
Yvan Janssens <yvanj@cyber.gent>
2022-04-04 11:04:01 +0200

    Change report action to declarative approach to migrate away from struts.xml

--- src/main/java/gent/cyber/energymon/actions/ReportAction.java
+++ src/main/java/gent/cyber/energymon/actions/ReportAction.java
@@ -5,14 +5,21 @@ import gent.cyber.energymon.models.EnergyPayment;
 import gent.cyber.energymon.models.MeterReading;
 import gent.cyber.energymon.utils.PaymentsUtil;
 import gent.cyber.energymon.utils.ReadingsUtil;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.List;
 
+@Namespace("/energymon")
 public class ReportAction extends ActionSupport {
     private transient final Logger log = Logger.getLogger(this.getClass().getName());
-    @Override
+
+    @Action(value = "report", results = {
+            @Result(name = SUCCESS, location = "/WEB-INF/report.jsp"),
+    })
     public String execute() {
         return SUCCESS;
     }
